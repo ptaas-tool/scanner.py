a998e928919a8ab6b33d050e6ef48c4b74a0a640
AmirH.Najafizadeh <najafizadeh21@gmail.com>
2023-08-08 09:35:41 +0330

    Create proto.go

---
+++ pkg/proto/proto.go
@@ -0,0 +1,13 @@
+package proto
+
+type Proto struct {
+    Date    string `json:"date"`
+    Name    string `json:"project_name"`
+    URL     string `json:"url"`
+    Score   int    `json:"score"`
+    FP      string `json:"fp"`
+    Args    string `json:"args"`
+    Summary string `json:"summary"`
+    NP      string `json:"negative_payloads"`
+    PP      string `json:"positive_payloads"`
+}
