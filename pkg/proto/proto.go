e6296d0de0010f880d13b177a5163b1403a3d9e1
AmirH.Najafizadeh <najafizadeh21@gmail.com>
2023-08-08 09:51:37 +0330

    Update proto.go

--- pkg/proto/proto.go
+++ pkg/proto/proto.go
@@ -1,13 +1,13 @@
 package proto
 
 type Proto struct {
-    Date    string `json:"date"`
-    Name    string `json:"project_name"`
-    URL     string `json:"url"`
-    Score   int    `json:"score"`
-    FP      string `json:"fp"`
-    Args    string `json:"args"`
-    Summary string `json:"summary"`
-    NP      string `json:"negative_payloads"`
-    PP      string `json:"positive_payloads"`
+    Date    string  `json:"date"`
+    Name    string  `json:"project_name"`
+    URL     string  `json:"url"`
+    Score   int     `json:"score"`
+    FP      string  `json:"fp"`
+    Args    string  `json:"args"`
+    Summary Summary `json:"summary"`
+    NP      string  `json:"negative_payloads"`
+    PP      string  `json:"positive_payloads"`
 }
