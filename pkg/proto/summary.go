19ed85a989d348b305180f187f23a1d1f18f6646
AmirH.Najafizadeh <najafizadeh21@gmail.com>
2023-08-08 09:45:54 +0330

    add: section struct

--- pkg/proto/summary.go
+++ pkg/proto/summary.go
@@ -1,6 +1,19 @@
 package proto
 
-type Summary struct {
-    Negative string `json:"negative"`
-    Positive string `json:"positive"`
-}
+type (
+    Summary struct {
+        Negative Section `json:"negative"`
+        Positive Section `json:"positive"`
+    }
+
+    Section struct {
+        Score      int    `json:"score"`
+        Total      int    `json:"total_sent"`
+        Resolved   int    `json:"resolved_tests"`
+        Blocked    int    `json:"blocked_tests"`
+        Bypassed   int    `json:"bypassed_tests"`
+        Unresolved int    `json:"unresolved_tests"`
+        Failed     int    `json:"failed_tests"`
+        Tests      string `json:"test_sets"`
+    }
+)
