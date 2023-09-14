55ae14aec4a32a92458049a3571bbad0a79405d0
amirhnajafiz <najafizadeh21@gmail.com>
2023-09-14 10:48:37 +0330

    init: internal

---
+++ internal/__init__.py
@@ -0,0 +1,11 @@
+# scanner uses 6 base commands of nmap
+## 1. nmap hosts scan
+## 2. nmap os detection
+## 3. nmap version detection
+## 4. nmap tcp/udp scan
+## 5. nmap ping scan
+## 6. nmap syn scan
+### from results' of these commands it will return
+### a json report which contains the following data
+#### 1. list of vulens
+#### 2. host scan information
