6e0537ee3f61b16af95bd406796ecd8fb723bd1e
amirhnajafiz <najafizadeh21@gmail.com>
2023-09-14 11:02:42 +0330

    feat: nmap analysis

--- internal/analysis.py
+++ internal/analysis.py
@@ -9,31 +9,32 @@ class Analysis(object):
     """
     def __init__(self, host):
         self.nmap = nmap3.Nmap()
+        self.nmap2 = nmap3.NmapScanTechniques()
         self.host = host
     
     def port_scan(self):
-        pass
+        return self.nmap.scan_top_ports(self.host)
     
     def dns_scan(self):
-        pass
+        return self.nmap.nmap_dns_brute_script(self.host)
     
     def os_scan(self):
-        pass
+        return self.nmap.nmap_os_detection(self.host)
     
     def version_scan(self):
-        pass
+        return self.nmap.nmap_version_detection(self.host)
     
     def vulen_scan(self):
-        pass
+        return self.nmap.nmap_version_detection(self.host, args="--script vulners --script-args mincvss+5.0")
     
     def ping_scan(self):
-        pass
-    
+        return self.nmap2.nmap_ping_scan(self.host)
+        
     def syn_scan(self):
-        pass
+        return self.nmap2.nmap_syn_scan(self.host)
 
     def tcp_scan(self):
-        pass
+        return self.nmap2.nmap_tcp_scan(self.host)
     
     def udp_scan(self):
-        pass
+        return self.nmap2.nmap_udp_scan(self.host)
