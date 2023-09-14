2e5372c62d7bce21593d231defd3a8dae0d7fe3d
amirhnajafiz <najafizadeh21@gmail.com>
2023-09-14 11:15:02 +0330

    add: base scanners

--- scanner.py
+++ scanner.py
@@ -1,11 +1,24 @@
 import argparse
+from internal.analysis import Analysis
+from internal.report import Report
 
 
+# parse input arguments
 parser = argparse.ArgumentParser(description="scanner script", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument("--host", help="target address")
 
 args = parser.parse_args()
 config = vars(args)
 
+# execute scanner
 if __name__ == "__main__":
-    print(config['host'])
+    r = Report()
+    
+    # create analysis class
+    an = Analysis(config['host'])
+    
+    # start scanning stages
+    print(an.port_scan()) # add vulens results to report
+    
+    # pring output
+    print(r.export())
