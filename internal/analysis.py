e4e5fb9d95f2808a80bd85dcf820dd35fe2cdf71
amirhnajafiz <najafizadeh21@gmail.com>
2023-09-23 10:59:35 +0330

    add: files export of each method

--- internal/analysis.py
+++ internal/analysis.py
@@ -17,41 +17,71 @@ class Analysis(object):
         v = []
         res = self.nmap.scan_top_ports(self.host)
         
-        for key in res:
-            if res[key]['osmatch']:
-                v.append('os version')
+        with open("port_scan.json", "w") as file:
+            file.write(json.dumps(res, indent=4))
         
         return v
     
     def dns_scan(self):
-        print(json.dumps(self.nmap.nmap_dns_brute_script(self.host), indent=2))
-        #self.nmap.nmap_dns_brute_script(self.host)
+        res = self.nmap.nmap_dns_brute_script(self.host)
+        
+        with open("dns_scan.json", "w") as file:
+            file.write(json.dumps(res, indent=4))
+        
         return []
     
     def os_scan(self):
-        #self.nmap.nmap_os_detection(self.host)
+        res = self.nmap.nmap_os_detection(self.host)
+        
+        with open("os_scan.json", "w") as file:
+            file.write(json.dumps(res, indent=4))
+        
         return []
     
     def version_scan(self):
-        #self.nmap.nmap_version_detection(self.host)
+        res = self.nmap.nmap_version_detection(self.host)
+        
+        with open("version_scan.json", "w") as file:
+            file.write(json.dumps(res, indent=4))
+        
         return []
     
     def vulen_scan(self):
-        #self.nmap.nmap_version_detection(self.host, args="--script vulners --script-args mincvss+5.0")
+        res = self.nmap.nmap_version_detection(self.host, args="--script vulners --script-args mincvss+5.0")
+        
+        with open("vulen_scan.json", "w") as file:
+            file.write(json.dumps(res, indent=4))
+        
         return []
     
     def ping_scan(self):
-        #self.nmap2.nmap_ping_scan(self.host)
+        res = self.nmap2.nmap_ping_scan(self.host)
+        
+        with open("ping_scan.json", "w") as file:
+            file.write(json.dumps(res, indent=4))
+        
         return []
         
     def syn_scan(self):
-        #self.nmap2.nmap_syn_scan(self.host)
+        res = self.nmap2.nmap_syn_scan(self.host)
+        
+        with open("syn_scan.json", "w") as file:
+            file.write(json.dumps(res, indent=4))
+        
         return []
 
     def tcp_scan(self):
-        #self.nmap2.nmap_tcp_scan(self.host)
+        res = self.nmap2.nmap_tcp_scan(self.host)
+        
+        with open("tcp_scan.json", "w") as file:
+            file.write(json.dumps(res, indent=4))
+            
         return []
     
     def udp_scan(self):
-        #self.nmap2.nmap_udp_scan(self.host)
+        res = self.nmap2.nmap_udp_scan(self.host)
+        
+        with open("udp_scan.json", "w") as file:
+            file.write(json.dumps(res, indent=4))
+        
         return []
